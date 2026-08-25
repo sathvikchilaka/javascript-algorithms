cee98f63b3bcc0e4684742efd75ea34b67fbfc72
sathvikchilaka-personal <cs34@iitbbs.ac.in>
2026-08-26 01:50:40 +0530

    Swap MemeListing components in App.jsx to use MemeListing2 with IntersectionObserver for improved infinite scroll functionality.

--- src/playground/js-dsa-frontendgeek/system-design/infinite-scroll/app.jsx
+++ src/playground/js-dsa-frontendgeek/system-design/infinite-scroll/app.jsx
@@ -1,5 +1,5 @@
-import { MemeListing1 as MemeListing } from './components/MemeListing1.jsx'; // Scroll Math
-// import { MemeListing2 as MemeListing } from './components/MemeListing2.jsx'; // IntersectionObserver
+// import { MemeListing1 as MemeListing } from './components/MemeListing1.jsx'; // Scroll Math
+import { MemeListing2 as MemeListing } from './components/MemeListing2.jsx'; // IntersectionObserver
 
 export default function App() {
   return (
